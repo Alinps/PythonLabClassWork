e89ed3b2e0037d12a201dcd709f536fba38e9a21
alin <alinps8406@gmail.com>
2025-09-17 16:16:39 +0530

    cw day 6

---
+++ AttendanceTrakingDay6cw.py
@@ -0,0 +1,16 @@
+attendance = [18,20,19,15,21]
+
+full_days = 0
+
+total_attendance = 0
+
+for student in attendance:
+    total_attendance +=student
+    if student>20:
+        full_days +=1
+        print("Full")
+    else:
+        print("Nnot full")
+        
+print("number of full days: ",full_days)
+print("total attendance for 5 days:",total_attendance)
